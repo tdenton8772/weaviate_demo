6bb16f097457280d66d3c8ff0a72a1fd354d2f19
Tyler Denton <tylerdenton@Tylers-MacBook-Pro.local>
2025-06-05 19:45:29 -0400

    Updated for semantic search

--- query_product.py
+++ query_product.py
@@ -4,6 +4,7 @@ import weaviate.classes.config as wvcc
 from weaviate.classes.config import ReferenceProperty
 import json
 from weaviate.classes.query import Filter
+from weaviate.classes.query import QueryReference
 
 client = weaviate.connect_to_local(
     port=8080,
@@ -15,11 +16,17 @@ client = weaviate.connect_to_local(
 
 try:
     category_collection = client.collections.get("Product")
-
-    for item in category_collection.iterator(include_vector=True):
-        print(item.uuid, item.properties)
-        print(item.uuid, item.vector)
-        exit(1)
-
+    x = 0
+    for item in category_collection.iterator(include_vector=True, 
+                                            return_references=QueryReference(
+                                                                link_on="category_reference",
+                                                                return_properties=["name"]
+                                                            )
+                                            ):
+        print(item.uuid, item.properties.get("name_title"))
+        print(item.uuid, item.references)
+        if x==10:
+            exit(1)
+        x+=1
 finally:
     client.close()
